87368d43e840410ab1ccb3d9d254ae5a9db6726e
muslimah27 <muslimahhh2703@gmail.com>
2022-08-09 11:36:25 +0700

    video 14

--- lib/app/data/controller/auth_controller.dart
+++ lib/app/data/controller/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -5,26 +6,54 @@ import 'package:google_sign_in/google_sign_in.dart';
 import '../../routes/app_pages.dart';
 
 class AuthController extends GetxController {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   Future signInWithGoogle() async {
-  // Trigger the authentication flow
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    UserCredential? _userCredential;
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-  // Obtain the auth details from the request
-  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
 
-  // Create a new credential
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-  );
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
 
-  print(googleUser!.email);
+    print(googleUser!.email);
 
-  // Once signed in, return the UserCredential
-  return await FirebaseAuth.instance.signInWithCredential(credential).then((value) => Get.offAllNamed(Routes.HOME));
-}
-Future logout() async{
-  await FirebaseAuth.instance.signOut();
-  Get.offAllNamed(Routes.LOGIN);
-}
+    // Once signed in, return the UserCredential
+    await FirebaseAuth.instance
+        .signInWithCredential(credential)
+        .then((value) => _userCredential = value);
+
+    CollectionReference users = firestore.collection('users');
+
+    final cekUser = await users.doc(googleUser.email).get();
+    if (!cekUser.exists) {
+      users.doc(googleUser.email).set({
+        'uid': _userCredential!.user!.uid,
+        'name': googleUser.displayName,
+        'email': googleUser.email,
+        'photo': googleUser.photoUrl,
+        'createdAt': _userCredential!.user!.metadata.creationTime.toString(),
+        'lastLoginAt':_userCredential!.user!.metadata.lastSignInTime.toString(),
+      });
+    }else{
+      users.doc(googleUser.email).set({
+        'lastLoginAt' : _userCredential!.user!.metadata.lastSignInTime.toString(),
+
+      });
+
+
+    }
+    Get.offAllNamed(Routes.HOME);
+  }
+
+  Future logout() async {
+    await FirebaseAuth.instance.signOut();
+    Get.offAllNamed(Routes.LOGIN);
+  }
 }
